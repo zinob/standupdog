4a34a0242d777d643bb58775b232fa7699092453
Simon Albinsson <simon@zensum.se>
2018-07-10 11:15:10 +0200

    Style fixes

--- ESP-upython/mqtt_printer.py
+++ ESP-upython/mqtt_printer.py
@@ -1,3 +1,4 @@
+# vim: set ts=3:sw=3
 from machine import Timer, UART, Pin
 from urequests import get
 from umqtt.robust import MQTTClient
@@ -5,7 +6,8 @@ import time, micropython
 
 class printMQ():
 	def __init__(s,default_interval=600,wait_function=None):
-		self.c = MQTTClient("dog", "ops-senpai.5z.fyi", port=30001, user="dog", password="x3lespxjdttiqjb5")
+		mqttpass=open("mqttpass.txt").read().strip()
+		self.c = MQTTClient("dog", "ops-senpai.5z.fyi", port=30001, user="dog", password=mqttpass)
 		c.set_callback(self._sub_cb)
 		c.connect()
 		c.subscribe(b"printme")
@@ -22,7 +24,4 @@ class printMQ():
 				c.wait_msg()
 		finally:
 			c.disconnect()
-
-Pin(0,Pin.IN,Pin.PULL_UP).irq(handler=start_webrepl, trigger=Pin.IRQ_FALLING)
-
 
